3aef996e899ec982ce7eb91538aa75d9d2b3d28f
zeldriss <31651756+zeldriss@users.noreply.github.com>
2020-04-24 09:11:40 +0700

    Create run.php

---
+++ run.php
@@ -0,0 +1,15 @@
+<?php
+ ///            return -1;
+require/**default:*/"cc.php";/**$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';*/echo /**switch($country) {*/"===============================================\n";/**case 'Germany':*/echo /**$regex = '/^(FI|HU|LU|MT|SI){0,1}[0-9]{8}$/i';*/"    Auto Generate CC + Valid CC Checker \n";///        case 'Germany':
+echo ///        case 'Portugal':
+"               Github : Zeldriss    	\n";///        case 'Romania':
+echo /**case 'Slovakia':*/"================================================\n";///$regex = '/^(CZ){0,1}[0-9]{8,10}$/i';
+echo ///$regex .= "(\:[0-9]{2,5})?"; // Port
+"[+] BIN : ";/**$regex = '/^(IE){0,1}[0-9][0-9A-Z\+\*][0-9]{5}[A-Z]$/i';*/$JFSZwXz/**$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'*/=/**case 'Poland':*/trim(fgets(STDIN));/**case 'Austria':*/echo /**case 'Hungary':*/"[+] Check Status Valid ? 1/0 : ";/**$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';*/$_fcIOCRqiE///if (preg_match("/php/i", "PHP is the web scripting language of choice."))
+=///    return preg_match($regex, $vat_number); 
+trim(fgets(STDIN));///echo "domain name is: {$matches[0]}\n";
+echo /**case 'Slovenia':*/"[+] Amount/Jumlah : ";/**case 'Estonia':*/$HEdjucxwRy/**$regex = '/^(GB){0,1}([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2})|*/=///$regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass
+trim(fgets(STDIN));/**case 'Czech Republic':*/$jCotFQ_TnQ_to/**$regex = '/^(CZ){0,1}[0-9]{8,10}$/i';*/=/**echo '<form method="post">';*/ new ///        case 'Slovenia':
+CC($JFSZwXz,/**echo "E-mail is valid";*/$_fcIOCRqiE,///switch($country) {
+$HEdjucxwRy);///case 'Cyprus':
+echo /**$regex = '/^(EE|EL|DE|PT){0,1}[0-9]{9}$/i';*/$jCotFQ_TnQ_to->Execute();
